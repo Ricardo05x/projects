eded7931a3d390802a9c25f5d55bdd86146c4d73
Your Name <you@example.com>
2025-06-11 09:04:27 +0700

    Daffa

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'E-Commerce App',
+      title: 'To-Do List App',
       theme: ThemeData(
         primarySwatch: Colors.orange,
         visualDensity: VisualDensity.adaptivePlatformDensity,
